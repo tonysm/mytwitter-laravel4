19df41c71a2ee2d0c2adea4571d90da61ebab976
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-03 14:47:44 -0300

    add flat-ui, stylizing a little bit

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,19 +1,29 @@
 @extends('layouts.default')
 
 @section('content')
-	{{ Form::open(array('route' => 'adduser', 'method' => 'POST', 'id' => 'add_users')) }}
-	{{ Form::token() }}
+	<div class="row">
+		<div class="span6">
+			<h3>Welcome to MyTwitter!</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, eveniet, mollitia aut maiores libero incidunt inventore nostrum saepe reprehenderit ipsum dolore perspiciatis est doloremque iusto provident enim dolorum dicta! Porro.</p>
+		</div>
 
-	{{ Form::label('name', 'Your name') }}
-	{{ Form::text('name', Input::old('name')) }}
-
-	{{ Form::label('email', 'Your e-mail') }}
-	{{ Form::text('email', Input::old('email')) }}
-
-	{{ Form::label('password', 'Your password') }}
-	{{ Form::password('password') }}
-
-	{{ Form::submit('Register') }}
-
-	{{ Form::close() }}
+		<div class="span4 offset2">
+			<h3>Join now!</h3>
+			{{ Form::open(array('route' => 'adduser', 'method' => 'POST', 'id' => 'add_users')) }}
+			{{ Form::token() }}
+			
+			{{ Form::label('name', 'Your name') }}
+			{{ Form::text('name', Input::old('name')) }}
+			
+			{{ Form::label('email', 'Your e-mail') }}
+			{{ Form::text('email', Input::old('email')) }}
+			
+			{{ Form::label('password', 'Your password') }}
+			{{ Form::password('password') }}
+			
+			{{ Form::submit('Register', array('class' => 'btn btn-primary btn-large')) }}
+			
+			{{ Form::close() }}
+		</div>
+	</div>
 @endsection
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,10 +3,44 @@
 <head>
 	<meta charset="UTF-8">
 	<title>MyTwitter with Laravel 4</title>
+	{{ Html::style('css/bootstrap.css') }}
+	{{ Html::style('css/flat-ui.css') }}
+	{{ Html::style('css/app.css') }}
+	@yield('styles')
 </head>
 <body>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, soluta, voluptatem expedita sit sequi vel blanditiis harum illum nemo nesciunt repellat a facilis voluptates officia neque vero laborum ullam porro!
-
-	@yield('content')
+	<header>
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li>
+								<a href="#">MyTwitter</a>
+							</li>
+							<li>
+								<a href="#">Messages</a>
+							</li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container main">
+		@yield('content')
+	</div>
+	<footer class="container-fluid">
+		<div class="row-fluid">
+			<p class="span6 offset3">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, esse, optio cupiditate at deleniti tenetur eum facere a eveniet natus officia ut amet libero commodi nisi placeat nesciunt dignissimos deserunt.
+			</p>
+		</div>
+	</footer>
 </body>
 </html>
