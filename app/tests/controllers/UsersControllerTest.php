8d2b1c49949b5560406e54d00b98b4d8e757cfa7
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-03 23:31:17 -0300

    add tests to login

--- app/tests/controllers/UsersControllerTest.php
+++ app/tests/controllers/UsersControllerTest.php
@@ -27,4 +27,33 @@ class UsersControllerTest extends TestCase
 
 		$this->assertRedirectedTo('/');
 	}
+
+	public function testLoginCorrectly()
+	{
+		$user = new Models\User();
+		$user->username = 'loremipsum';
+		$user->email = 'lorem@ipsum.com';
+		$user->password = '123123';
+		$user->password_confirmation = '123123';
+
+		$user->save();
+
+		$this->call('POST', 'login', array(
+			'username' => $user->username,
+			'password' => '123123'
+		));
+
+		$this->assertRedirectedToRoute('userhome');
+	}
+
+	public function testLoginViewIsDisplayedWhenLoginFails()
+	{
+		$this->call('POST', 'login', array(
+			'username' => 'lorem',
+			'password' => 'ipsum'
+		));
+
+		$this->assertViewHas('loginerrors');
+		$this->assertNull(Auth::user());
+	}
 }
