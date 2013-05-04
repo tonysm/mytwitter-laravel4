a040acbde27ca1773fb28f285302ff25cc399007
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-03 21:24:54 -0300

    add some styles for validation errors

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -2,31 +2,48 @@
 
 @section('content')
 	<div class="row">
-		<div id="errormessagess" class="span12">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ratione, recusandae quis voluptate enim non iusto quos provident repudiandae nihil officia tenetur rerum dolores eius vitae. Consectetur eum quis asperiores.
+		@if($errors->count())
+		<div id="errormessagess" class="span12 alert alert-error">
+			<p>Ooops, it seems you have made some mistakes.</p>
+			<ul>
+				{{ $errors->first('username', '<li>:message</li>') }}
+				{{ $errors->first('email', '<li>:message</li>') }}
+				{{ $errors->first('password', '<li>:message</li>') }}
+				{{ $errors->first('password_confirmation', '<li>:message</li>') }}
+			</ul>
 		</div>
+		@endif
 		<div class="span7">
 			<h3>Welcome to MyTwitter!</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, eveniet, mollitia aut maiores libero incidunt inventore nostrum saepe reprehenderit ipsum dolore perspiciatis est doloremque iusto provident enim dolorum dicta! Porro.</p>
+			<p>Oops, it seems you have made some mistakes.</p>
+			<ul>
+				{{ $errors->first('username', '<li>:message</li>') }}
+				{{ $errors->first('email', '<li>:message</li>') }}
+				{{ $errors->first('password', '<li>:message</li>') }}
+				{{ $errors->first('password_confirmation', '<li>:message</li>') }}
+			</ul>
 		</div>
 
 		<div class="span4 offset1">
 			<h3>Join now!</h3>
 			{{ Form::open(array('route' => 'adduser', 'method' => 'POST', 'id' => 'add_users')) }}
 			{{ Form::token() }}
-			
+			<div class="control-group {{ $errors->has('username') ? 'error' : '' }}">
 			{{ Form::label('username', 'Your name') }}
 			{{ Form::text('username', Input::old('username'), array('class' => 'span4', 'placeholder' => 'johndoe')) }}
-			
+			</div>
+			<div class="control-group {{ $errors->has('email') ? 'error' : '' }}">
 			{{ Form::label('email', 'Your e-mail') }}
 			{{ Form::text('email', Input::old('email'), array('class' => 'span4', 'placeholder' => 'johndoe@example.com')) }}
-			
+			</div>
+			<div class="control-group {{ $errors->has('password') ? 'error' : '' }}">
 			{{ Form::label('password', 'Your password') }}
 			{{ Form::password('password', array('class' => 'span4', 'placeholder' => '********')) }}
-			
+			</div>
+			<div class="control-group {{ $errors->has('password') ? 'error' : '' }}">
 			{{ Form::label('password_confirmation', 'Confirm your password') }}
 			{{ Form::password('password_confirmation', array('class' => 'span4', 'placeholder' => '********')) }}
-
+			</div>
 			{{ Form::submit('Register', array('class' => 'btn btn-primary btn-large btn-block')) }}
 			
 			{{ Form::close() }}
