6a0b1b2a024dbf8a571953df06469c27a25d699e
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-05 15:02:38 -0300

    add friends migration

---
+++ app/database/migrations/2013_05_05_171408_create_friends_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('friends', function( $table ) {
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->integer('friend_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('friends');
+	}
+
+}
