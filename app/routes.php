4ced58b9965086d6c0ef01e94235e89f706c83b8
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-05 15:04:08 -0300

    add routes

--- app/routes.php
+++ app/routes.php
@@ -1,22 +1,18 @@
 <?php
 
 Route::get('/', 'HomeController@index');
-Route::get('users', array(
-	'before' => 'auth',
-	'as' => 'userhome',
-	'uses' => 'UsersController@index'
-));
-Route::get('login', array(
-	'as' => 'login',
-	'uses' => 'UsersController@login'
-));
+Route::get('login', array('as' => 'login','uses' => 'UsersController@login'));
 
-Route::post('users', array(
-	'before' => 'csrf',
-	'as' => 'adduser',
-	'uses' => 'UsersController@create'
-));
-Route::post('login', array(
-	'before' => 'csrf',
-	'uses' => 'UsersController@doLogin'
-));
+Route::group(array('before' => 'csrf'), function() {
+	Route::post('users', array('as' => 'adduser', 'uses' => 'UsersController@create'));
+	Route::post('login', array('uses' => 'UsersController@doLogin'));
+});
+
+Route::group(array('before' => 'auth'), function() {
+	Route::get('users/find', array('as' => 'findusers', 'uses' => 'UsersController@findUsers'));
+	Route::get('users', array('as' => 'userhome','uses' => 'UsersController@index'));
+});
+
+Route::group(array('before' => array('auth', 'csrf')), function() {
+	Route::post('users/find', array('uses' => 'UsersController@findUsers'));
+});
