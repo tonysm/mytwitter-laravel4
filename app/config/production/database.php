6380386472ec9610861a595c85cacf52488abe75
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-05-15 17:41:28 -0300

    changing production configs to fit travis

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -1,28 +1,12 @@
 <?php
 
 return array(
-	'fetch' => PDO::FETCH_CLASS,
-	'default' => 'mysql',
+	'default' => 'sqlite',
 	'connections' => array(
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => $_SERVER["DB1_HOST"],
-			'database'  => $_SERVER["DB1_NAME"],
-			'username'  => $_SERVER["DB1_USER"],
-			'password'  => $_SERVER["DB1_PASS"],
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'port' 		=> $_SERVER["DB1_PORT"]
+		'sqlite' => array(
+			'driver' => 'sqlite',
+			'database' => ':memory:',
+			'prefix' => ''
 		)
-	),
-	'migrations' => 'migrations',
-	'redis' => array(
-		'cluster' => true,
-		'default' => array(
-			'host'     => '127.0.0.1',
-			'port'     => 6379,
-			'database' => 0,
-		),
-	),
+	)
 );
